ea2d96881dbcd1572e8755f080ad359b1dae9b06
SavionLegendZzz <saviondeavault@gmail.com>
2015-10-06 16:55:38 -0400

    Create Main.php

---
+++ src/STG/Main.php
@@ -0,0 +1,27 @@
+namespace STG;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\command\ConsoleCommandSender;
+
+class Main extends PluginBase implements Listener
+{
+public function onEnable() {
+		
+        
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		
+		}
+		
+		 public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+        if(strtolower($cmd->getName()) === "wu") {
+            if(isset($args[0])) {
+                $name = $args[0];
+               //brb gonna eat
